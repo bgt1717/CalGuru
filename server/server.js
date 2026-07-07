c0c3b83f51c1c6a844653c7f233c98c94946a8fd
bgt1717 <brycetown10@gmail.com>
2026-07-06 21:14:56 -0500

    middleware and server update

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -11,7 +12,8 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/authRoutes"));
+// Routes
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
