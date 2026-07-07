eba40da3489f546220aa9fa9c2213ce7f8c6b1d1
bgt1717 <brycetown10@gmail.com>
2026-07-06 20:42:26 -0500

    auth controller update

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,28 +2,14 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  });
-};
-
-const register = async (req, res) => {
+// REGISTER
+exports.register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    if (!username || !email || !password) {
-      return res.status(400).json({
-        message: "Please fill in all fields",
-      });
-    }
-
     const existingUser = await User.findOne({ email });
-
     if (existingUser) {
-      return res.status(400).json({
-        message: "User already exists",
-      });
+      return res.status(400).json({ message: "Email already in use" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -35,47 +21,44 @@ const register = async (req, res) => {
     });
 
     res.status(201).json({
-      _id: user._id,
-      username: user.username,
-      email: user.email,
-      token: generateToken(user._id),
+      message: "User created successfully",
+      userId: user._id,
     });
   } catch (err) {
-    res.status(500).json({
-      message: err.message,
-    });
+    res.status(500).json({ message: err.message });
   }
 };
 
-const login = async (req, res) => {
+// LOGIN
+exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-
-    if (
-      user &&
-      (await bcrypt.compare(password, user.password))
-    ) {
-      return res.json({
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-        token: generateToken(user._id),
-      });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    res.status(401).json({
-      message: "Invalid credentials",
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
     });
   } catch (err) {
-    res.status(500).json({
-      message: err.message,
-    });
+    res.status(500).json({ message: err.message });
   }
-};
-
-module.exports = {
-  register,
-  login,
 };
