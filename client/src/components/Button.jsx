e72819b267f2a22245ac1afe3db476f52cbcc1bd
bgt1717 <brycetown10@gmail.com>
2026-07-07 22:30:16 -0500

    CSS Setup

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,19 @@
+import "./Button.css";
+
+export default function Button({
+    children,
+    onClick,
+    type="button",
+    variant="primary"
+}){
+
+    return(
+        <button
+            className={`btn btn-${variant}`}
+            onClick={onClick}
+            type={type}
+        >
+            {children}
+        </button>
+    );
+}
